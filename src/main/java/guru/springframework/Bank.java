56f9d85a5e554f009b143e599e7449289f27fc19
Jakub Vrana <jvrana@monetplus.cz>
2018-11-01 15:11:30 +0100

    tdd example

---
+++ src/main/java/guru/springframework/Bank.java
@@ -0,0 +1,27 @@
+package guru.springframework;
+
+import java.util.HashMap;
+
+/**
+ * Created by jt on 2018-10-07.
+ */
+class Bank {
+
+    private final HashMap<Pair, Integer> rateMap = new HashMap<>();
+
+    Money reduce(Expression source, String toCurrency) {
+        return source.reduce(this, toCurrency);
+    }
+
+    int rate(String from, String to) {
+        if (from.equals(to)){
+            return 1;
+        }
+        return rateMap.get(new Pair(from, to));
+    }
+
+    void addRate(String from, String to, int rate) {
+        rateMap.put(new Pair(from, to), rate);
+    }
+
+}
---
+++ src/main/java/guru/springframework/Money.java
@@ -0,0 +1,52 @@
+package guru.springframework;
+
+public class Money implements Expression {
+
+    private final String currency;
+    final int amount;
+
+    public Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+
+    String currency() {
+        return currency;
+    }
+
+    static Money dollar(int amount) {
+        return new Money(amount, "USD");
+    }
+
+    static Money franc(int amount) {
+        return new Money(amount, "CHF");
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        Money money = (Money) o;
+        return amount == money.amount && currency == money.currency;
+    }
+    @Override
+    public Money reduce(Bank bank, String to) {
+        return new Money(amount / bank.rate(this.currency, to), to);
+    }
+
+    @Override
+    public String toString() {
+        return "Money{" +
+                "currency='" + currency + '\'' +
+                ", amount=" + amount +
+                '}';
+    }
+
+    @Override
+    public Expression times(int multiplier) {
+        return new Money(amount * multiplier, this.currency);
+    }
+
+    @Override
+    public Expression plus(Expression addend) {
+        return new Sum(this, addend);
+    }
+}
